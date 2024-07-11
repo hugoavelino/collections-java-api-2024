5e171f65dc65a3cccee054c252f2aaf978064df1
Hugo <hugo.motta2010@gmail.com>
2024-07-11 19:04:25 -0300

    Fim aula list intarface

---
+++ src/main/java/list/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,49 @@
+package main.java.list.Ordenacao;
+
+import java.util.*;
+
+public class OrdenacaoPessoa {
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("Nome 1", 20, 1.56);
+        ordenacaoPessoa.adicionarPessoa("Nome 2", 30, 1.80);
+        ordenacaoPessoa.adicionarPessoa("Nome 3", 25, 1.70);
+        ordenacaoPessoa.adicionarPessoa("Nome 4", 17, 1.56);
+
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+
+    }
+
+}
+
+class ComparatorPorAltura implements Comparator<Pessoa>{
+
+    @Override
+    public int compare(Pessoa p1, Pessoa p2) {
+        return Double.compare(p1.getAltura(), p2.getAltura());
+    }
+}
--- src/main/java/list/Pesquisa/CatalagoLivros.java
+++ src/main/java/list/Pesquisa/CatalagoLivros.java
@@ -52,5 +52,18 @@ public class CatalagoLivros {
         return livroPorTitulo;
     }
 
+    public static void main(String[] args) {
+        CatalagoLivros catalogoLivros = new CatalagoLivros();
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1",2020);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2",2021);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2",2022);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3",2023);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4",1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 2"));
+        System.out.println(catalogoLivros.pesquisaPorIntervaloAnos(2020, 2021));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+    }
+
 
 }
